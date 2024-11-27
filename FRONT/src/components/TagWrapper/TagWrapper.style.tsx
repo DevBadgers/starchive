a39a4bdd274e671392962a0d733c2278bec265e1
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-27 22:21:13 +0900

    feat: tagWrapper 컴포넌트 구현(#5)

---
+++ FRONT/src/components/TagWrapper/TagWrapper.style.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Tag = styled.div`
+  display: inline-block;
+  padding: 2px 5px;
+  font-size: 12px;
+  background-color: var(--sub-color);
+  color: var(--text-color);  // Fix the typo here
+  width: fit-content;
+  margin-right: 5px;
+  margin-bottom: 5px;
+`;
+
+export const TagList = styled.div`
+    display: inline-block;
+`;
---
+++ FRONT/src/components/TagWrapper/TagWrapper.tsx
@@ -0,0 +1,17 @@
+import { TagList, Tag } from "../TagWrapper/TagWrapper.style";
+
+interface TagProps {
+  tagList: string[];
+}
+
+function TagWrapper({ tagList }: TagProps) {
+  return (
+    <TagList>
+      {tagList.map((tag, index) => (
+        <Tag key={index}>{tag}</Tag>
+      ))}
+    </TagList>
+  );
+}
+
+export default TagWrapper;
