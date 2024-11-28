012bd3077d20f55c0732325687ff10837c0c5978
THANH HIEN DOAN <thanhhiendoan@THANHui-noteubug.local>
2024-11-28 16:48:56 +0900

    feat: Navbar PC 화면 구현

--- FRONT/src/components/Navbar/Navbar.style.ts
+++ FRONT/src/components/Navbar/Navbar.style.ts
@@ -12,6 +12,10 @@ export const ToggleIcon = styled.img`
     width: 24px;
     height: 24px;
     cursor: pointer;
+
+    @media (min-width: 768px) {
+        display: none;
+    }
 `;
 
 export const ProfileIcon = styled.img`
@@ -20,3 +24,37 @@ export const ProfileIcon = styled.img`
     border-radius: 50%;
     cursor: pointer;
 `;
+
+export const ImageWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    h4 {
+        color: #fff;
+    }
+
+    img {
+        width: 24px;
+        height: 24px;
+    }
+`;
+
+export const MenuWrapper = styled.article`
+    display: flex;
+    align-items: center;
+    gap: 30px;
+    
+    h4 {
+        color: #fff;
+        cursor: pointer;
+    }
+    
+    h4:hover {
+        color: var(--point-color);
+    }
+    
+    @media (max-width: 768px) {
+        display: none;
+    }
+}`;
--- FRONT/src/components/Navbar/Navbar.tsx
+++ FRONT/src/components/Navbar/Navbar.tsx
@@ -1,11 +1,21 @@
 import exampleProfile from "../../assets/images/example-profile.svg";
 import toggleIcon from "../../assets/icons/toggle-icon.svg";
-import { NavbarContainer, ToggleIcon, ProfileIcon } from "./Navbar.style";
+import logoIcon from "../../assets/logo/logo.svg";
+import { NavbarContainer, ToggleIcon, ProfileIcon, ImageWrapper, MenuWrapper } from "./Navbar.style";
 
 function Navbar() {
   return (
     <NavbarContainer>
       <ToggleIcon src={toggleIcon} alt="toggle" />
+      <MenuWrapper>
+        <ImageWrapper>
+          <img src={logoIcon} alt="toggle" />
+          <h4><strong>Starchive</strong></h4>
+        </ImageWrapper>
+        <h4>메뉴 1</h4>
+        <h4>메뉴 2</h4>
+        <h4>메뉴 3</h4>
+      </MenuWrapper>
       <ProfileIcon src={exampleProfile} alt="profile" />
     </NavbarContainer>
   );
