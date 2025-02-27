7af4aff8697a22b4c3f7682c5d0841baea03019b
hanguswls <hanguswls@naver.com>
2025-02-28 01:22:51 +0900

    refactor: Navbar의 프로필 아이콘을 로그인 버튼으로 변경
    - 로그인 버튼 클릭 시 로그인 페이지("/signin")로 이동
    - 임시로 ProfileIcon을 주석 처리함
    - Refs: #95

--- FRONT/src/components/Navbar/Navbar.style.ts
+++ FRONT/src/components/Navbar/Navbar.style.ts
@@ -1,22 +1,43 @@
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const NavbarContainer = styled.section`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 20px;
-    background-color: var(--primary-color);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: var(--primary-color);
 `;
 
 export const ToggleIcon = styled.img`
-    width: 24px;
-    height: 24px;
-    cursor: pointer;
+  width: 24px;
+  height: 24px;
+  cursor: pointer;
 `;
 
 export const ProfileIcon = styled.img`
-    width: 35px;
-    height: 35px;
-    border-radius: 50%;
-    cursor: pointer;
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+export const SignInLinkWrapper = styled.div`
+  height: 35px;
+  align-content: center;
+`;
+
+export const SignInLink = styled(Link)`
+  all: unset;
+  display: block;
+  color: white;
+  padding: 4px 8px 2px 8px;
+  cursor: pointer;
+  transition: transform 0.2s ease-in-out;
+
+  &:hover,
+  &:active {
+    background-color: var(--sub-color);
+    color: var(--text-color);
+  }
 `;
